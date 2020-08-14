153e237df4ce394bee3e1555d70d65c6f42fb74a
Md. Tarek <moh.tarek00003@gmail.com>
2020-08-14 23:53:06 +0600

    ext5

--- src/main/java/com/masterdevskills/cha1/ext5/DidThatWork.java
+++ src/main/java/com/masterdevskills/cha1/ext5/DidThatWork.java
@@ -8,12 +8,32 @@ public class DidThatWork {
         User user = new User("Bazlur", "Rahman");
         // logger.info("user status:  {}", getUserStatus(user));
         logger.info("user status:  {}", () -> new String[]{getUserStatus(user)});
+        logger.trace("Tracking Id:  {}", () -> new String[]{getTrackingId()});
+        logger.debug("Debug:  {}", () -> new String[]{getDebugResult()});
+        logger.warn("show warn:  {}", () -> new String[]{showWarn()});
     }
 
     private static String getUserStatus(final User user) {
         System.out.println("Preparing user status");
 
         return user.toString();
+    }
+    private static String getTrackingId() {
+        System.out.println("Inside Trace method");
+
+        return "1234";
+    }
+
+    private static String getDebugResult() {
+        System.out.println("Inside Debug method");
+
+        return "Debug List";
+    }
+
+    private static String showWarn() {
+        System.out.println("Inside warn method");
+
+        return "warning state";
     }
 
     static class User {
